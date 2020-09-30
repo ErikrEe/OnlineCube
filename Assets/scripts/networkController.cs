b9b11b4f013ad332f3e1a0ff7a0dc9f7b678c706
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-09-30 10:30:27 +0200

    Button

--- Assets/scripts/networkController.cs
+++ Assets/scripts/networkController.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class networkController : MonoBehaviourPunCallbacks
 {
+
+    [SerializeField]
+    Button startButton;
+    [SerializeField]
+    InputField nameField;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        startButton.interactable = false;
         PhotonNetwork.ConnectUsingSettings();
+        print("Connecting...");
     }
 
     public override void OnConnectedToMaster()
     {
-        print("connected to server PhotonNetwork");
+        print("connected to server in " + PhotonNetwork.CloudRegion);
+        startButton.interactable = true;
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     // Update is called once per frame
