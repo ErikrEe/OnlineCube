4397b5716b1d0fbdb31804465e8b29061249634f
Gustangle <Gustangle@users.noreply.github.com>
2020-10-01 14:53:26 +0200

    generisk kod
    generisk kod, i form av en text syns i unity-konsolen.

---
+++ Assets/scripts/genericcode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class genericcode : MonoBehaviour
+{
+    public U GenericMethod<U>(U value1, U value2)
+    {
+        print(value1);
+        return value2;
+    }
+    void Start()
+    {
+        string name;
+        name = "welcome to 2020's game of the year";
+        print(name);
+    }
+}
